c7831d49add48eb75779dea867528126ff9f9dee
Oluwadara Oloye <daraoloye99@gmail.com>
2023-02-09 22:04:18 +0100

    CHORE: Create mobile layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.scss';
+import backOfCreditCard from './images/bg-card-back.png'
+import frontOfCreditCard from './images/bg-card-front.png'
 
 function App() {
+  const windowWidth = useState(window.innerWidth)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="app">
+      <div className="top">
+        <div className="back-of-credit-card-container">
+          <img src={backOfCreditCard} className="back-of-credit-card" alt="Back of Credit Card" />
+        </div>
+        <div className="front-of-credit-card-container">
+          <div style={{ position: 'relative' }}>
+            <img src={frontOfCreditCard} className="front-of-credit-card" alt="Front of Credit Card" />
+            <div className="front-details">
+              <div className="top-details">
+                <div className="circle big-circle"></div>
+                <div className="circle small-circle"></div>
+              </div>
+              <div className="bottom-details">
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bottom"></div>
+    </main>
   );
 }
 
