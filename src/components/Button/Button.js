729d7be879ee161a98820635eec8c68652711a7f
Oluwadara Oloye <daraoloye99@gmail.com>
2023-02-16 01:01:42 +0100

    CHORE: Add logic to show error messages

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,9 @@
 import './Button.scss'
 
-const Button = ({ type = 'button', btnText, handleClick = () => {} }) => {
+const Button = ({ type = 'button', btnText, handleClick = () => {}, disabled = false }) => {
+  console.log('Check Disabled =>', disabled)
   return (
-    <button className='btn' type={type} onClick={handleClick}>{ btnText }</button>
+    <button className='btn' type={type} onClick={handleClick} disabled={disabled}>{ btnText }</button>
   )
 }
 
--- src/components/DateInput/DateInput.js
+++ src/components/DateInput/DateInput.js
@@ -1,13 +1,44 @@
+import { useState } from 'react'
 import './DateInput.scss'
+function DateInput({ label, handleMonthOnChange, handleYearOnChange, getErrorStatus }) {
+  const [monthError, setMonthError] = useState(false)
+  const [yearError, setYearError] = useState(false)
+  const monthCheck = (event) => {
+    (event.target.value.length >= 2) ? setMonthError(false) : setMonthError(true)
+    handleMonthOnChange(event)
+    console.log(`CHECK 1 => ${monthError} - ${yearError}`)
+    getErrorStatus(monthError && yearError)
+  }
+  const yearCheck = (event) => {
+    (event.target.value.length >= 2) ? setYearError(false) : setYearError(true)
+    handleYearOnChange(event)
+    console.log(`CHECK 2 => ${monthError} - ${yearError}`)
+    getErrorStatus(monthError && yearError)
 
-function DateInput({ label, handleMonthOnChange, handleYearOnChange }) {
+  }
   return (
     <>
       <div className="inputs-container">
         <label className='label-text'>{ label }</label> <br/>
         <div className="inputs">
-          <input type="text" className='month-input' onChange={handleMonthOnChange} placeholder='MM' />
-          <input type="text" className='year-input' onChange={handleYearOnChange} placeholder='YY'/>
+          <input 
+            type="text" 
+            className='month-input' 
+            onChange={monthCheck} 
+            placeholder='MM' 
+            onBlur={monthCheck}
+            onFocus={monthCheck}
+            style={{ border: monthError ? '.1rem solid red' : '.1rem solid #cdcdcd'  }}
+          />
+          <input 
+            type="text" 
+            className='year-input' 
+            onChange={yearCheck} 
+            placeholder='YY'
+            onBlur={yearCheck}
+            onFocus={yearCheck}
+            style={{ border: yearError ? '.1rem solid red' : '.1rem solid #cdcdcd'  }}
+          />
         </div>
       </div>
     </>
